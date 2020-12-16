23151dc3530557eb426b3286ee5e33c32d111cda
Bruno Rossini <brunorossini@live.com>
2020-12-15 22:23:31 -0300

    build script

--- dist/middlewares/error.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _expressvalidation = require('express-validation'); var _expressvalidation2 = _interopRequireDefault(_expressvalidation);
-
-exports. default = (err, req, res, next) => {
-  if (err instanceof _expressvalidation2.default.ValidationError) {
-    return res.status(err.status).json(err);
-  }
-
-  return next();
-};
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "bmfit",
-      script: "src/server.js",
+      script: "dist/server.js",
       instances: "1",
       autorestart: true,
       watch: false,
