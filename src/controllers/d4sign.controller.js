449b4beaa8a26becdabac52552880564a0d6bc65
Bruno Rossini <brunorossini@live.com>
2021-01-30 12:45:02 -0300

    d4sign webhook test

--- src/controllers/d4sign.controller.js
+++ src/controllers/d4sign.controller.js
@@ -1,11 +1,9 @@
 export default {
   index: async (req, res) => {
-    console.log(req.body);
-
     res.json({ message: "webhook d4sign" });
   },
   create: async (req, res) => {
-    console.log(req.body);
+    console.log(`post:d4sign`, req.body);
 
     res.json({ message: "webhook d4sign" });
   },
