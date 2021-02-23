e628657f96d6c10f51aff7ed7c34508624527c65
Bruno Rossini <brunorossini@live.com>
2021-02-23 16:41:14 -0300

    config deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,15 +17,15 @@ module.exports = {
   ],
   deploy: {
     production: {
-      host: 'aws-ronaldo', /// configure aws-geo no arquvio ~/.ssh/config
-      user: 'bitname',
-      ref: 'origin/master',
-      repo: 'git@github.com:brunorossini/bmfit.git',
-      path: '/home/bitname/bmfit',
-      'post-deploy':
-        'npm install && sucrase ./src -d ./dist --transforms imports && pm2 start ecosystem.config.js --env production',
+      host: "aws-ronaldo", /// configure aws-geo no arquvio ~/.ssh/config
+      user: "bitname",
+      ref: "origin/master",
+      repo: "git@github.com:brunorossini/bmfit.git",
+      path: "/home/bitnami/bmfit",
+      "post-deploy":
+        "npm install && sucrase ./src -d ./dist --transforms imports && pm2 start ecosystem.config.js --env production",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
     },
   },
