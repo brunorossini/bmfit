c292ef1b4e3d3e3d9db85c773814a39af4f11180
Bruno Rossini <bruno.rossini@pagar.me>
2021-03-11 14:53:11 -0300

    feature: availables schedules

--- src/controllers/available.controller.js
+++ src/controllers/available.controller.js
@@ -29,39 +29,39 @@ export default {
       },
     });
 
-    return res.json(appointments);
+    const schedule = [
+      "08:00",
+      "09:00",
+      "10:00",
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+    ];
 
-    // const schedule = [
-    //   "08:00",
-    //   "09:00",
-    //   "10:00",
-    //   "11:00",
-    //   "12:00",
-    //   "13:00",
-    //   "14:00",
-    //   "15:00",
-    //   "16:00",
-    //   "17:00",
-    //   "18:00",
-    //   "19:00",
-    // ];
+    console.log(appointments.find((a) => format(a.date, "HH:mm")));
 
-    // const available = schedule.map((time) => {
-    //   const [hour, minute] = time.split(":");
-    //   const value = setSeconds(
-    //     setMinutes(setHours(searchDate, parseInt(hour)), minute),
-    //     0
-    //   );
+    const available = schedule.map((time) => {
+      const [hour, minute] = time.split(":");
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, parseInt(hour)), minute),
+        0
+      );
 
-    //   return {
-    //     time,
-    //     value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-    //     available:
-    //       isAfter(value, new Date()) &&
-    //       !appointments.find((a) => format(a.date, "HH:mm") === time),
-    //   };
-    // });
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find((a) => format(a.date, "HH:mm") === time),
+      };
+    });
 
-    // return res.json(available);
+    return res.json(available);
   },
 };
